a698f0627d54cc05f7c897194fac5316a1a127ac
Keisuke Sakai <penguin22231@gmail.com>
2021-03-14 01:09:45 +0900

    DBを変更

--- lagoinfo/settings.py
+++ lagoinfo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,12 +79,9 @@ WSGI_APPLICATION = 'lagoinfo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+db_from_env = dj_database_url.config()
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.splite3',
-    }
+    'default': dj_database_url.config()
 }
 
 
