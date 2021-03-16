0db959bcaf872a3b8f302433530af3e9fea5d2b2
Keisuke Sakai <penguin22231@gmail.com>
2021-03-16 22:52:49 +0900

    IPアドレスの取り方を変更

--- muta/decorators.py
+++ muta/decorators.py
@@ -4,7 +4,12 @@ from lagoinfo.settings import ALLOWED_IP_BLOCKS
 
 def ip_checker(func):
     def check_ip(request, *args, **kwargs):
-        if request.META['REMOTE_ADDR'] not in ALLOWED_IP_BLOCKS:
-            return HttpResponseNotFound("Not Allowed to Access from {}!".format(request.META['REMOTE_ADDR']))
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        if ip not in ALLOWED_IP_BLOCKS:
+            return HttpResponseNotFound("Not Allowed to Access from {}!".format(ip))
         return func(request, *args, **kwargs)
     return check_ip
