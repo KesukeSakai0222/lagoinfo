d587d812147799d9edd4abba96dd7585568dba6e
Keisuke Sakai <penguin22231@gmail.com>
2021-03-16 22:44:16 +0900

    デバッグモードを終了

--- lagoinfo/settings.py
+++ lagoinfo/settings.py
@@ -25,9 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['lagoinfo.herokuapp.com',
+    'lagoinfo.com',
+    ]
 
 
 # Application definition
